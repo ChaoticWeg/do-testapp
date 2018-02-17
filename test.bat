23ce7ae1fe26014011c778466c225e08ed165b22
Shawn Lutch <shawn@chaoticweg.cc>
2018-02-17 00:13:20 -0600

    bugfix: set breaks when hh, mm, or ss is 00
    shh bby is ok

--- test.bat
+++ test.bat
@@ -4,13 +4,13 @@ cls
 setlocal
 
     set hh=%time:~0,2%
-    if %hh:~0,1% == " " set hh=0%hh:~1,1%
+    if "%hh:~0,1%" == " " set hh=0%hh:~1,1%
 
     set mm=%time:~3,2%
-    if %mm:~0,1% == " " set mm=0%mm:~1,1%
+    if "%mm:~0,1%" == " " set mm=0%mm:~1,1%
 
     set ss=%time:~6,2%
-    if %ss:~0,1% == " " set ss=0%ss:~1,1%
+    if "%ss:~0,1%" == " " set ss=0%ss:~1,1%
 
     echo Start: %date% at %hh%:%mm%:%ss%
     echo.
