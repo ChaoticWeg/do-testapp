33fb80103134d160ee4b351cb83b3548587e7ad9
Shawn Lutch <shawn.lutch94@gmail.com>
2018-02-23 15:31:03 -0600

    print environment type
    just to be sure

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const App = require('./lib/app');
 
 const PORT = process.env['PORT'] || 3000;
 
+console.log('running in %s mode', process.env.NODE_ENV);
+
 var app = App.getInstance();
 app.listen(PORT, () => console.log('listening on port %d', PORT));
 
